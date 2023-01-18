9b4f9a588790eaf9efa7319a4cf1c7491d095b2a
HasanNajjar <haasaan9@hotmail.com>
2023-01-18 09:52:37 +0300

    vefveer

--- src/main/java/com/bbk/api/apiservice/ApiserviceApplication.java
+++ src/main/java/com/bbk/api/apiservice/ApiserviceApplication.java
@@ -1,13 +1,28 @@
 package com.bbk.api.apiservice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class ApiserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiserviceApplication.class, args);
+	}
+
+	@GetMapping("/version")
+	public Map<String, String> versionCheck() {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("version", "VERSION_NUMBER");
+
+		return map;
 	}
 
 }
